c04fdaa96edef19e7fe20efc18d5b83b2e955107
LaGo55 <84121213+LaGo55@users.noreply.github.com>
2022-12-01 13:49:43 +0100

    gui

--- DataToCSV.py
+++ DataToCSV.py
@@ -1,12 +1,10 @@
 import csv
-import functionConditionMonitoring as fCM
 fieldnames = ["Time","Data_1","Data_2"]  #,"Data_3"
 with open('data.csv', 'w', newline='') as csv_file:
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames, dialect="excel")
     csv_writer.writeheader()
 
 def CreateDataBase(x_value, sensor_data_1, sensor_data_2):
-
 
     with open('data.csv', 'a', newline='') as csv_file:
         csv_writer = csv.DictWriter(csv_file,fieldnames=fieldnames, dialect="excel")

0f0b221f5c104deab03e0119d47d46634d5dd039
LaGo55 <84121213+LaGo55@users.noreply.github.com>
2022-12-01 14:06:32 +0100

    gui

--- initiate.py
+++
@@ -1,8 +0,0 @@
-# Skript zum initialisieren der benötigten Variablen
-
-import serial
-
-i = 0
-com = "com6"    #Arduino Port
-ArduinoData = serial.Serial(com, 9600) #Arduino Port und Bau-Rate
-statuscolor = "#7CFC00"  #hellgrün

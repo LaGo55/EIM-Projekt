0d649bec9673dbc72c7e4cc8f095314a8193f471
TimStb <117913205+TimStb@users.noreply.github.com>
2022-12-15 13:11:55 +0100

    Update ArduinoProgramm_20221213.ino

--- ArduinoProgramm_20221213.ino
+++ ArduinoProgramm_20221213.ino
@@ -1,22 +1,49 @@
+#include <SparkFun_LIS331.h>
+#include <Wire.h>
 #include <SoftwareSerial.h>
-SoftwareSerial BT(10, 11); 
-char a; // variable to store messages
-void setup()  
+String message;
+#define HWSERIAL Serial
+//#define tx 0
+//#define rx 1
+
+LIS331 xl;
+
+void setup() 
 {
-  BT.begin(9600);                    // set the data rate for the SoftwareSerial port
-  BT.println("Hello from Arduino");  // send wellcome message to other device
+  HWSERIAL.begin(9600);
+  Wire.begin();
+  xl.setI2CAddr(0x19);    // This MUST be called BEFORE .begin() so 
+                          //  .begin() can communicate with the chip
+  xl.begin(LIS331::USE_I2C); // Selects the bus to be used and sets
+                          //  the power up bit on the accelerometer.
+                          //  Also zeroes out all accelerometer
+                          //  registers that are user writable.
+  LIS331::NORMAL;
+  LIS331::DR_1000HZ;
+  Serial.begin(9600);
 }
+
 void loop() 
 {
-  // if text arrived in from BT serial...
-  if (BT.available()){
-    a=(BT.read());             // store incoming character from other device
-    if (a=='1'){               //when send character 1 then...
-      BT.println("send 1");
-    } else if (a=='2'){        //when send character 2 then...
-      BT.println("send 2");
-    } else {                   // otherwise print this description...
-      BT.println("Send '1' or '2'");
-    }   
+  int16_t x, y, z;
+    xl.readAxes(x, y, z);  // The readAxes() function transfers the
+                           //  current axis readings into the three
+                           //  parameter variables passed to it.
+    //Serial.println(x);
+    //Serial.println(y);
+    //Serial.println(z);
+    //Serial.println(xl.convertToG(6,x)); // The convertToG() function
+    //Serial.println(xl.convertToG(6,y)); // accepts as parameters the
+    Serial.println(xl.convertToG(6,z)); // raw value and the current
+    //Serial.println(" ");                // maximum g-rating.
+
+      while(HWSERIAL.available())
+  {//while there is data available on the serial monitor
+    message+=char(HWSERIAL.read());//store string from serial command
   }
-}
+  if(message!="")
+  {
+      Serial.println(">" + message); //show the data
+      HWSERIAL.println(message); //show the data
+    }
+  }
